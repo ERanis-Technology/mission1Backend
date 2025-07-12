92a005368dc055160c7751c30a780b7c0f8f4bbd
HEIL-TCHAMBA-NANA <htchamba124@gmail.com>
2025-07-12 12:14:33 +0100

    Definition des modeles de donnees Django pour les profils

--- plateformeynno/settings.py
+++ plateformeynno/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'profils',
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
